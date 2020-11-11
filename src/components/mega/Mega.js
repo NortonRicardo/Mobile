d52d167005dbb42e3ce4ccdd64652643e237ae6e
Norton Ricardo <norton.ricardo@passwordinfo.com.br>
2020-11-11 14:19:45 -0300

    Mega 1

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Text, TextInput } from 'react-native'
+import { Button, Text, TextInput } from 'react-native'
 import Estilo from '../estilo'
 
 export default class Mega extends Component {
     
     state={ 
-        qtNumeros: this.props.qtNumeros
+        qtNumeros: this.props.qtNumeros,
+        numeros: []
     }
 
     // constructor(props){
@@ -13,10 +14,26 @@ export default class Mega extends Component {
     //     this.alterarQtdeNumero = this.alterarQtdeNumero.bind(this)
     // }
 
+    // alterarQtdeNumero(qtde){
+    //     this.setState({qtNumeros: qtde})
+    // }
+
     alterarQtdeNumero = (qtde) => {
-        this.setState({qtNumeros: qtde})
+        this.setState({qtNumeros: +qtde})
+    }
+    
+    gerarNumeroNaoContido = nuns =>{
+        const novo = parseInt(Math.random() * 60)+1
+        return nuns.includes(novo) ? this.gerarNumeroNaoContido(nuns) : novo
     }
 
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtNumeros)
+                        .fill()
+                        .reduce(nuns => [...nuns, this.gerarNumeroNaoContido(nuns)], [])
+                        .sort((a, b) => a - b)
+        this.setState({numeros: numeros})
+    }
     
     render(){
         return(
@@ -24,10 +41,7 @@ export default class Mega extends Component {
                 <Text style={Estilo.fontG}>
                     Gerador de Mega-Sena 
                 </Text>
-                <Text style={Estilo.fontM}>
-                    {this.state.qtNumeros}
-                </Text>
-
+                
                 <TextInput 
                     keyboardType={ 'numeric'}
                     style={ {borderWidth: 1, marginTop: 7, borderRadius: 10, padding: 10}}
@@ -35,6 +49,15 @@ export default class Mega extends Component {
                     value ={this.state.qtNumeros}
                     onChangeText={this.alterarQtdeNumero}
                 />
+
+                <Button
+                title="Gerar"
+                style={ {marginTop: 7, padding: 10} }
+                onPress={this.gerarNumeros} />
+
+                <Text style={Estilo.fontM}>
+                    {this.state.numeros.join(' -- ')}
+                </Text>
             </>
         )
     }
